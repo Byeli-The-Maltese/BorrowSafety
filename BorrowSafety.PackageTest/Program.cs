c12929dfc2227271134768ded3c485b3ffc081ad
Byeli-The-Maltese <danielvanderbilt@protonmail.com>
2026-06-07 10:24:25 -0500

    Figured out how to pack the analzyer into a nuget package so consumers of the package may use it.

---
+++ BorrowSafety.PackageTest/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace BorrowSafety.PackageTest;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Borrow<FileInfo> borrow = new FileInfo("Program.cs");
+        string name = borrow.Name;
+        Console.WriteLine(name);
+    }
+}
